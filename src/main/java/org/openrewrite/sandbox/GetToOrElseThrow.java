88b072f5150fc0a4d54c55c739a81d0e9a811fa6
AlekSimpson <alek@asu.me>
2023-01-06 13:35:42 -0700

    learned about builder templates and method matchers

--- src/main/java/org/openrewrite/sandbox/GetToOrElseThrow.java
+++ src/main/java/org/openrewrite/sandbox/GetToOrElseThrow.java
@@ -3,6 +3,7 @@ package org.openrewrite.sandbox;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.tree.J;
@@ -10,21 +11,23 @@ import org.openrewrite.java.tree.J;
 import java.util.Optional;
 
 public class GetToOrElseThrow extends Recipe {
-    public static final MethodMatcher IMMUTABLE_LIST_MATCHER = new MethodMatcher("java.util.Optional get()");
-
     @Override
     public String getDisplayName() { return "Replace Optional.get with Optional::orElseThrow"; }
 
     @Override
     protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        final MethodMatcher IMMUTABLE_LIST_MATCHER = new MethodMatcher("java.util.Optional get()");
+        final JavaTemplate orElseThrowTemplate  =
+                JavaTemplate.builder(this::getCursor,"Optional.of("").orElseThrow(NoSuchElementException::new)")
+                        .build();
         return new JavaVisitor<ExecutionContext>() {
-            @Override
-            public J vistMethodInnvocation(J.MethodInvocation method, ExecutionContext executionContext){
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext){
                 if (IMMUTABLE_LIST_MATCHER.matches(method)) {
 
 
                 }
+                return method;
             }
-        }
+        };
     }
 }
